7634054fc06dc8be14f560be986da9ec6f7aa883
ULIVZ <472590061@qq.com>
2018-07-09 22:24:43 +0800

    first commit

---
+++ register-service-worker.js
@@ -0,0 +1,95 @@
+// Modified from: https://github.com/yyx990803/register-service-worker
+// Register a service worker to serve assets from local cache.
+
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+  // [::1] is the IPv6 localhost address.
+  window.location.hostname === '[::1]' ||
+  // 127.0.0.1/8 is considered localhost for IPv4.
+  window.location.hostname.match(
+    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+  )
+)
+
+function register (swUrl, hooks) {
+  const emit = (hook, ...args) => {
+    if (hooks && hooks[hook]) {
+      hooks[hook](...args)
+    }
+  }
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      if (isLocalhost) {
+        // This is running on localhost, let's check if a service worker still exist or not.
+        checkValidServiceWorker(swUrl, emit)
+        navigator.serviceWorker.ready.then(() => {
+          emit('ready')
+        })
+      } else {
+        // It's not local host. Just register service worker.
+        registerValidSW(swUrl, emit)
+      }
+    })
+  }
+}
+
+function registerValidSW (swUrl, emit) {
+  navigator.serviceWorker
+    .register(swUrl)
+    .then(registration => {
+      emit('registered', registration)
+      const installingWorker = registration.installing
+      registration.onupdatedfound = () => {
+        if (installingWorker.state === 'installed') {
+          // At this point, the old content will have been purged and
+          // the fresh content will have been added to the cache,
+          // It's the perfect time to display a "New content is
+          // available; please refresh." message in your web app.
+          emit('updated', registration)
+        } else {
+          // At this point, everything have been precached.
+          // It's the perfect time to display a
+          // "Content is cached for offline use." message.
+          emit('cached', registration)
+        }
+      }
+    })
+    .catch(error => {
+      emit('error', error)
+    })
+}
+
+function checkValidServiceWorker (swUrl, emit) {
+  // Check if a service worker can be found
+  fetch(swUrl)
+    .then(response => {
+      // Ensure service worker exists, and that we really are getting a JS file.
+      if (
+        response.status === 404 ||
+        response.headers.get('content-type').indexOf('javascript') === -1
+      ) {
+        // No service worker found/
+        emit('error', new Error(`Service worker not found at ${swUrl}`))
+        unregister()
+      } else {
+        // Service worker found. Proceed as normal.
+        registerValidSW(swUrl, emit)
+      }
+    })
+    .catch(error => {
+      if (!navigator.onLine) {
+        emit('offline')
+      } else {
+        emit('error', error)
+      }
+    })
+}
+
+function unregister () {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(registration => {
+      registration.unregister()
+    })
+  }
+}
---
+++ sw.js
@@ -0,0 +1,73 @@
+console.warn('[Service Worker] v2')
+
+const CACHE_NAMESPACE = 'ulivz'
+const PRECACHE = CACHE_NAMESPACE + 'precache-v1'
+const PRECACHE_LIST = [
+  './offline.html'
+]
+
+const RUNTIME = CACHE_NAMESPACE + 'runtime-v1'
+const expectedCaches = [PRECACHE, RUNTIME]
+
+const LOG_STYLE = 'background: #f66; color: #ffffff'
+const FECTH_FILE_STYLE = 'background: #222; color: #bada55'
+
+console.log('[SW] precache id: ' + PRECACHE)
+
+self.oninstall = (event) => {
+  console.log('[SW] %cinstalled', LOG_STYLE)
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_LIST))
+      .then(self.skipWaiting())
+      .catch(err => console.log(err))
+  )
+}
+
+self.onactivate = (event) => {
+  console.log('[SW] %cactivated', LOG_STYLE)
+
+  // delete any cache not match expectedCaches for migration.
+  // noticed that we delete by cache instead of by request here.
+  // so we MUST filter out caches opened by this app firstly.
+  // check out sw-precache or workbox-build for an better way.
+  event.waitUntil(
+    caches.keys().then(cacheNames => Promise.all(
+      cacheNames
+        .filter(cacheName => cacheName.startsWith(CACHE_NAMESPACE))
+        .filter(cacheName => !expectedCaches.includes(cacheName))
+        .map(cacheName => caches.delete(cacheName))
+    ))
+  )
+}
+
+self.onfetch = event => {
+  const cached = caches.match(event.request)
+  const fixedUrl = `${event.request.url}?${Date.now()}`
+  const fetched = fetch(fixedUrl, { cache: 'no-store' })
+  const fetchedCopy = fetched.then(resp => resp.clone())
+  const url = new URL(event.request.url)
+
+  console.log(`[SW] Fetch %c ${url.pathname}`, FECTH_FILE_STYLE)
+
+  // Intercept Request
+  if (url.origin === location.origin && url.pathname === '/ulivz.png') {
+    return event.respondWith(caches.match('evan.jpeg'))
+  }
+
+  // Call respondWith() with whatever we get first.
+  // If the fetch fails (e.g disconnected), wait for the cache.
+  // If there’s nothing in cache, wait for the fetch.
+  // If neither yields a response, return offline pages.
+  event.respondWith(
+    Promise.race([fetched.catch(_ => cached), cached])
+      .then(resp => resp || fetched)
+      .catch(_ => caches.match('offline.html'))
+  )
+
+  event.waitUntil(
+    Promise.all([fetchedCopy, caches.open(RUNTIME)])
+      .then(([response, cache]) => response.ok && cache.put(event.request, response))
+      .catch(_ => { /* ignore errors */ })
+  )
+}
